4227b9f5c658702fb89c5fa7a4435238e41a88cc
DANIEL21NAULA <163338712+DANIEL21NAULA@users.noreply.github.com>
2025-11-12 17:53:34 -0500

    Implements Proyect

---
+++ src/cars/cars.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { CarsController } from './cars.controller';
+import { CarsService } from './cars.service';
+
+@Module({
+  controllers: [CarsController],
+  providers: [CarsService], //* todos los servicios son providers, pero no todos los providers son servicios
+})
+export class CarsModule {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: 2,
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: 3,
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+  ];
+}
